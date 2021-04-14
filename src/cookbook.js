e5dbfb801eec37f66c7b3b89b4cc3f116d2cc4e4
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-04-13 20:15:06 -0400

    Add CookBook and basic operations

---
+++ src/cookbook.js
@@ -0,0 +1,23 @@
+class CookBook {
+  constructor() {
+    this.recipes = {};
+  }
+
+  addRecipe(name, ingredients) {
+    this.recipes[name] = ingredients;
+  }
+
+  listRecipes() {
+    return Object.keys(this.recipes);
+  };
+
+  getRecipe(name) {
+    return this.recipes[name];
+  }
+
+  removeRecipe(name) {
+    delete this.recipes[name];
+  }
+}
+
+module.exports = { CookBook };
