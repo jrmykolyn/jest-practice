01d9eaff3046d2c0530f69e14c87ea1696f210da
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-04-13 20:36:11 -0400

    Add CookBookCli

---
+++ src/cookbook-cli.js
@@ -0,0 +1,37 @@
+const { CookBook } = require('./cookbook');
+
+class CookBookCli {
+  constructor() {
+    this.cookbook = new CookBook();
+  }
+
+  run(command, ...args) {
+    switch (command) {
+      case 'list': return this.list();
+      case 'add': return this.add(...args);
+      case 'get': return this.get();
+      case 'remove': return this.remove();
+      default: return `Whoops, the following command is unsupported: ${command}.`;
+    }
+  }
+
+  list() {
+    return `You have the following recipes: ${this.cookbook.listRecipes().join(',')}`;
+  }
+
+  add(name, ingredients) {
+    this.cookbook.addRecipe(name, ingredients);
+    return `Successfully added the following recipe: ${name}`;
+  }
+
+  get(name) {
+    return `The ingredients for ${name} are: ${this.cookbook.getRecipe(name)}`;
+  }
+
+  remove(name) {
+    this.cookbook.removeRecipe(name);
+    return `Successfully removed the following recipe: ${name}`;
+  }
+}
+
+module.exports = { CookBookCli };
