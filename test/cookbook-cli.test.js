70c821864e38857ad68c791e0d185ccb0e02ce77
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-04-13 21:40:56 -0400

    chore: Change 'it()' to 'test()'

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -3,25 +3,25 @@ const { CookbookCli } = require('../src/cookbook-cli');
 
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
-    it('should accept the recipe information and display the correct message', () => {
+    test('should accept the recipe information and display the correct message', () => {
 
     });
   });
 
   describe('Listing recipes', () => {
-    it('should display the correct message listing all of the recipe names', () => {
+    test('should display the correct message listing all of the recipe names', () => {
 
     });
   });
 
   describe('Retrieving a recipe', () => {
-    it('should display the ingredients required to make the specified recipe', () => {
+    test('should display the ingredients required to make the specified recipe', () => {
 
     });
   });
 
   describe('Deleting a recipe', () => {
-    it('should accept the recipe name and display the correct message', () => {
+    test('should accept the recipe name and display the correct message', () => {
 
     });
   });
--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -2,25 +2,25 @@ const { Cookbook } = require('../src/cookbook');
 
 describe('Cookbook', () => {
   describe('Adding recipes', () => {
-    it('should allow a recipe to be added', () => {
+    test('should allow a recipe to be added', () => {
 
     });
   });
 
   describe('Listing recipes', () => {
-    it('should allow the recipes to be listed', () => {
+    test('should allow the recipes to be listed', () => {
 
     });
   });
 
   describe('Retrieving a recipe', () => {
-    it('should allow the ingredients for a recipe to be retrieved', () => {
+    test('should allow the ingredients for a recipe to be retrieved', () => {
 
     });
   });
 
   describe('Deleting a recipe', () => {
-    it('should allow a recipe to be deleted', () => {
+    test('should allow a recipe to be deleted', () => {
 
     });
   });
