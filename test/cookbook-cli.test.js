ea26daebf9549ec2c4c2f9546be7549a9dc6b7b2
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-04-13 21:11:33 -0400

    Define test files and structure

---
+++ test/cookbook-cli.test.js
@@ -0,0 +1,28 @@
+const { CookBook } = require('../src/cookbook');
+const { CookBookCli } = require('../src/cookbook-cli');
+
+describe('CookBookCli', () => {
+  describe('Adding recipes', () => {
+    it('should accept the recipe information and display the correct message', () => {
+
+    });
+  });
+
+  describe('Listing recipes', () => {
+    it('should display the correct message listing all of the recipe names', () => {
+
+    });
+  });
+
+  describe('Retrieving a recipe', () => {
+    it('should display the ingredients required to make the specified recipe', () => {
+
+    });
+  });
+
+  describe('Deleting a recipe', () => {
+    it('should accept the recipe name and display the correct message', () => {
+
+    });
+  });
+});
---
+++ test/cookbook.test.js
@@ -0,0 +1,27 @@
+const { CookBook } = require('../src/cookbook');
+
+describe('CookBook', () => {
+  describe('Adding recipes', () => {
+    it('should allow a recipe to be added', () => {
+
+    });
+  });
+
+  describe('Listing recipes', () => {
+    it('should allow the recipes to be listed', () => {
+
+    });
+  });
+
+  describe('Retrieving a recipe', () => {
+    it('should allow the ingredients for a recipe to be retrieved', () => {
+
+    });
+  });
+
+  describe('Deleting a recipe', () => {
+    it('should allow a recipe to be deleted', () => {
+
+    });
+  });
+});
